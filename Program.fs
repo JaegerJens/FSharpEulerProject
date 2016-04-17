d5c7969e566807caacbc69e80d83842aa3de548f
Jens Jaeger <jaegerj204@googlemail.com>
2016-04-17 12:28:34 +0200

    Problem 2 solution

--- Program.fs
+++ Program.fs
@@ -4,7 +4,7 @@ module Main =
     open System
         [<EntryPoint>]
         let main argv = 
-            Problem1.result
-                |> printfn "Problem 1 result: %A"
+            Problem1.result |> printfn "Problem 1 result: %A"
+            Problem2.result |> printfn "Problem 2 result: %A"
             Console.ReadKey() |> ignore
             0 // return an integer exit code
